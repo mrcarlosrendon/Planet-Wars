5dc34e387d08527d7c8e3a8d5ec7de7ed7365878
Carlos Rendon <crendon@gmail.com>
2010-09-30 20:54:03 -0700

    Added KILL KILL KILL logic, still crashing, not as bad as before

--- MyBot.py
+++ MyBot.py
@@ -29,8 +29,8 @@ def BreakEvenTurns(planet, fleetDistance):
   """
   if planet.GrowthRate() == 0:
     return 10000
-  return ceil(FleetRequiredToTake(planet, fleetDistance) + \
-         planet.NumShips() / float(planet.GrowthRate()))  
+  return int(ceil(FleetRequiredToTake(planet, fleetDistance) + \
+         planet.NumShips() / float(planet.GrowthRate())))
 
 def FleetRequiredToTake(planet, fleetDistance):
   """Returns the exact size of a fleet required to take the given
@@ -38,9 +38,9 @@ def FleetRequiredToTake(planet, fleetDistance):
   """
   # TODO: take into accounts fleets already headed to the planet
   if planet.Owner() == 0: #neutral
-    return planet.NumShips()
+    return planet.NumShips() + 1
   else:
-    return planet.NumShips() + planet.GrowthRate()*fleetDistance  
+    return int(ceil(planet.NumShips() + planet.GrowthRate()*fleetDistance + 1))
 
 def DefenseRequired(pw, planet, enemies):
   defense = 0
@@ -49,7 +49,7 @@ def DefenseRequired(pw, planet, enemies):
   for enemy in enemies:
     defense += enemy.NumShips() - \
                rate*pw.Distance(enemy, planet)
-  defense = ceil(defense)
+  defense = int(ceil(defense))
   debug("DefenseRequired planet " + str(planet.PlanetID()) + " " + str(defense))
   return defense
 
@@ -60,14 +60,42 @@ def DoTurn(pw):
   neutralPlanets = pw.NeutralPlanets()
   myFleets = pw.MyFleets()
   enemyFleets = pw.EnemyFleets()
+  enemySize = 0
+  mySize = 0
 
-  # Figure out where I have excesss
+  # How am I doing?
   debug("status")
   for p in pw.MyPlanets():
+    mySize = mySize + p.NumShips()
     if p.NumShips() > .5*DefenseRequired(pw, p, enemyPlanets):
       defendedPlanets.append(p)
     else:
       vulnerablePlanets.append(p)
+
+  # How is the enemy doing?
+  for p in enemyPlanets:
+    enemySize = enemySize + p.NumShips()
+
+  winRatio = float(mySize)/enemySize
+  debug("Ratio: " + str(winRatio))
+
+  # Should I go for the kill?
+  if winRatio > 1.5:
+    debug("Kill Kill Kill!!!!")
+    for p in enemyPlanets:
+      required = FleetRequiredToTake(p, 100) # TODO fudging here for now
+      for mp in pw.MyPlanets():
+        if required > 0:
+          defenseReq = int(ceil(.10*DefenseRequired(pw, mp, enemyPlanets)))
+          toSend = mp.NumShips() - defenseReq
+          if mp.NumShips() - toSend > 0:          
+            pw.IssueOrder(mp, p, toSend)
+            logging.debug(str(mp.PlanetID()) + " sent " + str(toSend) + \
+                          " to " + str(p.PlanetID()))
+            mp.NumShips(mp.NumShips()-toSend)
+            logging.debug(str(mp.NumShips()) + " left")
+            required = required - toSend
+    return
       
   # Look for a good bargin
   debug("looking for bargins")
@@ -83,8 +111,11 @@ def DoTurn(pw):
       if attackedPlanets.count(p.PlanetID()) > 0:
         continue
       dist = pw.Distance(p, taker)
-      if BreakEvenTurns(p, dist) < 50:        
-        attackFleetSize = FleetRequiredToTake(p, dist) + ceil(.25*DefenseRequired(pw, p, enemyPlanets))
+      if BreakEvenTurns(p, dist) < 50:
+        defenseReq = int(ceil(.25*DefenseRequired(pw, p, enemyPlanets)))
+        attackFleetSize = FleetRequiredToTake(p, dist)
+        if defenseReq > 0:
+          attackFleetSize = attackFleetSize + defenseReq
         if attackFleetSize > 0 and taker.NumShips() > attackFleetSize: 
           logging.debug(str(taker.PlanetID()) + " sent " + str(attackFleetSize) + \
                         " to " + str(p.PlanetID()))
