88809a3b55bbb7a9b5f3ec6c2a56b15d8c8b5b06
Carlos Rendon <carlos_rendon@symantec.com>
2010-10-04 16:32:55 -0700

    lots of fleshing out from last WIP, there are bugs in adjusting for incoming fleets and also in how under agressive it is

--- MyBot.py
+++ MyBot.py
@@ -10,13 +10,21 @@
 """
 
 from PlanetWars import PlanetWars
+from PlanetWars import Planet
 from math import ceil
 from Log import debug
 
 distances = []
 nearestNeighbors = []
 
+def DebugOrder(attacker, to, amount, remaining):
+  debug(str(attacker.PlanetID()) + " sent " + \
+        str(amount) + " to " + str(to.PlanetID()) + \
+        ", " + str(remaining) + " remaining")
+
 def Distance(p1id, p2id):
+  if isinstance(p1id, Planet) and isinstance(p2id, Planet):
+    return distances[p1id.PlanetID()][p2id.PlanetID()][1]
   return distances[p1id][p2id][1]
 
 def ComputePlanetDistances(pw):
@@ -33,10 +41,21 @@ def BreakEvenTurns(pw, planet, fleetDistance):
   taking this planet.
   """
   if planet.GrowthRate() == 0:
-    debug("wtf, ZERO GROWTH")
-    return 10000
-  return int(ceil(FleetRequiredToTake(pw, planet, fleetDistance) + \
-         planet.NumShips() / float(planet.GrowthRate())))
+    debug("wtf, ZERO GROWTH on " + str(planet.PlanetID()))
+    return 100000
+  cost = FleetRequiredToTake(pw, planet, fleetDistance)
+  # if it is already being taken, then we won't break even
+  if cost <= 0:
+    debug(str(planet.PlanetID()) + " already being taken")
+    return 100000
+  if planet.Owner() == 2:
+    # enemy planets pay back in half the time
+    # because there is the growth that the enemy didn't get
+    # plus the growth that I did get
+    returnRate = 2*float(planet.GrowthRate())
+  else:
+    returnRate = float(planet.GrowthRate())
+  return int(ceil(cost / returnRate))
 
 def FleetRequiredToTake(pw, planet, fleetDistance):
   """Returns the exact size of a fleet required to take the given
@@ -46,20 +65,27 @@ def FleetRequiredToTake(pw, planet, fleetDistance):
   required = planet.NumShips() + 1
   for f in pw.Fleets():
     if f.DestinationPlanet() == planet.PlanetID():
-      adj = f.NumShips() - int(ceil(planet.GrowthRate()*f.TurnsRemaining()))
-      if f.Owner == 2 and adj > 0: # enemy
+      if planet.Owner() == 0: # if neutral, growth doesn't apply
+        adj = f.NumShips()
+      else:
+        adj = f.NumShips() - \
+              int(ceil(planet.GrowthRate()*f.TurnsRemaining()))
+      if f.Owner() == 2 and adj > 0: # enemy
         required += adj
-      if f.Owner == 1 and adj < 0: # mine
+      if f.Owner() == 1 and adj > 0: # mine
+        # adj has to be > 0 because otherwise sending this fleet is
+        # somehow making it harder to take.. that doesn't make sense
         required -= adj            
   if planet.Owner() == 2: # enemy 
-    required += int(ceil(planet.GrowthRate()*fleetDistance + 1))    
+    required += int(ceil(planet.GrowthRate()*fleetDistance + 1))
+  #debug(str(required) + " required to take " + str(planet.PlanetID()))
   return required
 
 def GeneralDefenseRequired(pw, planet):
   """How many reserves do I need if the nearest enemies send everything"""
   defense = 0
   rate = planet.GrowthRate()
-  neighbors = nearestNeightbors[planet.PlanetID()]
+  neighbors = nearestNeighbors[planet.PlanetID()]
   for n in neighbors:
     # If enemy and can hit me in 15 turns or less
     p = pw.GetPlanet(n[0])
@@ -72,6 +98,14 @@ def GeneralDefenseRequired(pw, planet):
 def DefenseRequiredForIncoming(pw, planet):
   """How many reserves do I need to leave to protect me from the
   incoming waves?"""
+
+
+
+
+  # I NEED HELP - THIS IS OBVIOUSLY BROKEN
+
+
+  
   defense = 0
   rate = planet.GrowthRate()
   for f in pw.EnemyFleets():
@@ -132,58 +166,71 @@ def DoTurn(pw):
   # some urgent planets are more urgent
   urgentPlanets.sort(key=lambda x: x[0].GrowthRate(), reverse=True)  
   for helpme in urgentPlanets:
-    needed = helpme[1]
+    totalNeeded = helpme[1]
     helpsofar = 0
     plannedSend = []
     # Make a defense plan. Prefer close help
     for helper in sorted(myPlanets, key=lambda x: Distance(x[0],helpme[0])):
-      # Don't undefend yourself
-      tosend = helper[0].NumShips() - helper[1]
+      # Don't undefend yourself      
+      tosend = helper[0].NumShips()
+      defReq = helper[1]
+      if defReq > 0:
+        tosend -= defReq
       if tosend <= 0:
         continue        
-      # Only send what is needed
-      if tosend > needed:
-        tosend = needed
-      plannedSend.append((helper[0].PlanetID(), helpme[0].PlanetID(), tosend))
+      # Only send what is still needed
+      stillNeeded = totalNeeded - helpsofar
+      if tosend > stillNeeded:
+        tosend = stillNeeded
+      plannedSend.append((helper[0], helpme[0], tosend))
       helpsofar += tosend
-      if helpsofar >= needed:
+      if helpsofar >= totalNeeded:
         break
     # issue actual orders
-    if helpsofar >= needed:
+    if helpsofar >= totalNeeded:
       for p in plannedSend:
-        pw.IssueOrder(p)
+        pw.IssueOrder(p[0], p[1], p[2])
+        remaining = p[0].NumShips()-p[2]
+        DebugOrder(p[0], p[1], p[2], remaining)
         orders.append(p)
-        sender = pw.GetPlanet(p[0])
-        sender.NumShips(sender.NumShips()-p[2])
+        p[0].NumShips(remaining)
     # else, TODO: this planet is BONED until I fix
 
   # Maximize investments, Minimize enemy
+  targets = enemyPlanets+neutralPlanets
   debug("Offense")
   for taker in myPlanets:
     debug(str(taker[0].PlanetID()))
-
-    defenseReq = max(DefenseRequiredForIncoming(pw, p), \
-                       int(ceil(.50*GeneralDefenseRequired(pw, p, enemyPlanets))))
-
-    # Figure out what is most valueble for this taker
-    for p in enemyPlanets+neutralPlanets:
+    defenseReq = max(DefenseRequiredForIncoming(pw, taker[0]), \
+                       int(ceil(.50*GeneralDefenseRequired(pw, taker[0]))))
+    surplus = taker[0].NumShips()
+    if defenseReq > 0:
+      surplus -= defenseReq
+    potTargets = []    
+    # Calculate investement risks for this taker
+    for p in targets:
       dist = Distance(p.PlanetID(), taker[0].PlanetID())
       breakEvenTurns = BreakEvenTurns(pw, p, dist)
-      # how to create score from these three parameters?
-      
-
-      
-        if defenseReq > 0:
-          attackFleetSize = attackFleetSize + defenseReq
-        if attackFleetSize > 0 and taker.NumShips() > attackFleetSize: 
-          debug(str(taker.PlanetID()) + " sent " + str(attackFleetSize) + \
-                        " to " + str(p.PlanetID()))
-          pw.IssueOrder(taker, p, attackFleetSize)
-          taker.NumShips(taker.NumShips()-attackFleetSize)
-          attackedPlanets.append(p.PlanetID())
-          break
-        else:
-          debug("not enough ships")
+      potTargets.append((p, breakEvenTurns))
+    # Prefer lower risk, take as many targets as possible
+    potTargets.sort(key=lambda x: x[1])
+    for potTarget in potTargets:
+      needed = FleetRequiredToTake(pw, potTarget[0], \
+                                    Distance(potTarget[0].PlanetID(), p.PlanetID()))
+      # take if we can
+      if needed > 0 and surplus > needed:
+        pw.IssueOrder(taker[0], potTarget[0], needed)
+        remaining = taker[0].NumShips() - needed
+        DebugOrder(taker[0], potTarget[0], needed, remaining)
+        taker[0].NumShips(remaining)
+        surplus -= needed
+        # remove this from the targets list
+        for t in targets:
+          if t.PlanetID() == potTarget[0].PlanetID():
+            targets.remove(t)
+            break
+      elif surplus <= 0:
+        break
 
 def main():
   debug("starting")
